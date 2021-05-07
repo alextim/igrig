cbd757951e2c12a1a1e58184c4eed9b382310ae1
Alex Tim <buildmnt@gmail.com>
2021-05-07 22:59:09 +0300

    sm

--- src/components/Footer/LegalInfo/LegalInfo.jsx
+++ src/components/Footer/LegalInfo/LegalInfo.jsx
@@ -10,13 +10,16 @@ const style = {
   gridArea: 'legal',
   color: colors.footer.text,
   textDecoration: 'none',
+
   ':hover': {
     color: colors.footer.highlight,
   },
+
   ':active, :hover, :focus': {
     outline: 0,
     textDecoration: 'none',
   },
+
   [mq.lg]: {
     marginRight: '3rem',
   },
