f163f3dfe7c9f5696ff067ee7f95a9a5742dbf0d
Alex Tim <buildmnt@gmail.com>
2022-01-18 21:00:22 +0200

    createBlogPages

--- src/gatsby/create-pages.js
+++ src/gatsby/create-pages.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
 const path = require('path');
-const { createPages } = require('@alextim/at-blog');
-// const { createPages } = require('../../plugins/at-blog');
+const { createBlogPages } = require('@alextim/at-site-core');
+
 
 const { cardsPerPage, templatesDir } = require('../../igrig.content/config/website');
 const i18n = require('../i18n/i18n');
@@ -39,7 +38,7 @@ module.exports = async ({ graphql, actions, reporter }) => {
 
   ['photo-serie', 'photo-project'].forEach((type) => {
     const edges = result.data.posts.edges.filter(({ node }) => node.type === type);
-    createPages({
+    createBlogPages({
       edges,
 
       type,
