871b20b0579da2c9dd1b6e6efe8fbc09b1bec35e
Alex Tim <buildmnt@gmail.com>
2022-01-30 20:41:37 +0200

    err: onCreateWebpackConfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,2 +1,13 @@
+const path = require('path');
+
 exports.createPages = require('./src/gatsby/create-pages');
-exports.onCreateWebpackConfig = require('./src/gatsby/on-create-webpack-config');
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  });
+};
